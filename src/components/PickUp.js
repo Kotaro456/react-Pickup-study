8b45823f44fe5a98d5c55634342358d004c8e48d
Kotaro456 <kotaro03281616@gmail.com>
2020-06-27 08:58:40 +0900

    made PickUp function of simply

--- src/components/PickUp.js
+++ src/components/PickUp.js
@@ -1,11 +1,56 @@
 import React from 'react';
 
 class PickUp extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            // 日時をこれでゲット　toLocalTimeStringで国別の日時を文字列で返す
+            currentTime : new Date().toLocaleTimeString(),
+        };
+
+        this.pickUpSubject = this.pickUpSubject.bind(this);
+    }
+
+    // タイマーをセットするときはcomponentDidMount()、componentDidUpdate()はダメだった
+    componentDidMount() {
+        // setIntervalで1秒ごとに、currentTimeを更新するpassOneSecond()を行う
+        this.timeId = setInterval(() => {
+            this.passOneSecond();
+        }, 1000)
+    }
+
+    passOneSecond (){
+        this.setState({currentTime: new Date().toLocaleTimeString()})
+    }
+
+    componentWillMount() {
+        // clearIntervalはsetIntervalの解除を行う
+        clearInterval(this.timeId);
+    }
+
+    // ここまで現在時刻表示
+
+    // ランダムに教科を選ぶ
+    pickUpSubject() {
+        // ここのコードもう少しきれいにできる気がする
+
+        const randomSubject = this.props.subjects[
+            // ランダムに配列の中から選ぶ
+            Math.floor(Math.random() * this.props.subjects.length)
+        ];
+
+        const randomStudyTime = this.props.studyTime[
+             Math.floor(Math.random() * this.props.studyTime.length)
+        ]
+
+        alert(`${randomSubject}を${randomStudyTime}`);
+    }
     render() {
         return (
             <div className="pickup">
-              <p className="pickup__pickup-time">時間：00:00:00</p>
-              <button className="pickup__pickup-button">何を勉強する？</button>
+              <p className="pickup__pickup-time">{this.state.currentTime}</p>
+              <button onClick={this.pickUpSubject} className="pickup__pickup-button">何を勉強する？</button>
             </div>
         );
     }
--- src/components/PickUpStudy.js
+++ src/components/PickUpStudy.js
@@ -10,13 +10,19 @@ import AddOption from './AddOption';
 class PickUpStudy extends React.Component {
     constructor(props) {
         super(props);
-        
+        this.state = {
+            subjects: ["数学", "国語", "理科", "英語", "社会"],
+            studyTime: ["10min", "20min", "40min", "60min", "80min"],
+        }
     }
     render() {
         return (
             <div className="study">
             <Header />
-              <PickUp />
+              <PickUp
+               subjects={this.state.subjects}
+               studyTime={this.state.studyTime}
+               />
               <Options />
               <AddOption />
             </div>

b03af75aa445268e0eebfa3fb20008dc573c8526
Kotaro456 <kotaro03281616@gmail.com>
2020-06-29 02:11:58 +0900

    renew AddOption

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,45 +1,60 @@
 import React from "react";
 
 class AddOption extends React.Component {
-    constructor(props){
-        super(props);
-        this.addOption = this.addOption.bind(this);
+  constructor(props) {
+    super(props);
+    this.addOption = this.addOption.bind(this);
 
-        this.state = {
-          errorMessage : ''
-        }
+    this.state = {
+      errorMessage: "",
+    };
+  }
+
+  addOption(event) {
+    {
+      /**ユーザーが行うイベントのため　event.preventDefault();いれた方がいいかも */
     }
+    event.preventDefault();
 
-   addOption(event) {
-       {/**ユーザーが行うイベントのため　event.preventDefault();いれた方がいいかも */}
-       event.preventDefault();
+    {
+      /**入力されたテキストを取得                            trim()で入力値の前後の空白取り除く */
+    }
+    const textValue = event.target.elements.textBox.value.trim();
 
-       {/**入力されたテキストを取得                            trim()で入力値の前後の空白取り除く */}
-       const textValue = event.target.elements.textBox.value.trim();
+    const subjects = this.props.subjects
+    {
+      /**PickUpStudyコンポーネントから受け取ったメソッド発動 */
+    }
+    {
+      /**ifの条件式で入力フォームが空じゃないこと確認 */
+    }
+    if (textValue !== "") {
+      if (subjects.indexOf(textValue) > -1) {
+        this.setState({ errorMessage: "すでに同じ教科があります" });
+      } else {
+        this.props.addOption(textValue);
+        this.setState({ errorMessage: "" });
+      }
 
-       {/**PickUpStudyコンポーネントから受け取ったメソッド発動 */}
-       {/**ifの条件式で入力フォームが空じゃないこと確認 */}
-       if(textValue !== ''){
-          this.props.addOption(textValue);
-          this.setState({ errorMessage: '' });
-
-          // addOptionでstateにデータ設置できたら、入力フォームを空にする
-          event.target.elements.textBox.value = '';
-       }else if(textValue === '') {
-          this.setState({ errorMessage: '何か入力してください' });
-       }
-   }
-
+      // addOptionでstateにデータ設置できたら、入力フォームを空にする
+      event.target.elements.textBox.value = "";
+    } else if (textValue === "") {
+      this.setState({ errorMessage: "何か入力してください" });
+    }
+  }
 
   render() {
     return (
       <div>
-        {this.state.errorMessage !== '' && <p>{this.state.errorMessage}</p>}
-        <form  className="addoption" name="form" id="add_from" onSubmit={this.addOption}>
-          <input className="addoption__textbox" name='textBox'  type="text" />
-          <button className="addoption__button">
-            追加
-          </button>
+        {this.state.errorMessage !== "" && <p>{this.state.errorMessage}</p>}
+        <form
+          className="addoption"
+          name="form"
+          id="add_from"
+          onSubmit={this.addOption}
+        >
+          <input className="addoption__textbox" name="textBox" type="text" />
+          <button className="addoption__button">追加</button>
         </form>
       </div>
     );

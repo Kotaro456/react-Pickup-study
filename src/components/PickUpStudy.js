b9301275322c83498aa16615c60673b82148c268
Kotaro456 <kotaro03281616@gmail.com>
2020-06-29 04:32:03 +0900

    used lifecycle methods and localStorage

--- src/components/PickUpStudy.js
+++ src/components/PickUpStudy.js
@@ -44,6 +44,37 @@ class PickUpStudy extends React.Component {
         this.setState((prevState) => ({ subjects: prevState.subjects.concat([subject]) }));
     }
 
+
+    // これはページを更新してもデータが残る処理
+    // ほかのライフサイクルメソッドのcomponentWillMountが将来的に変更されるらしいい
+    // ページが表示される前の処理
+    componentDidMount() {
+        // localStorageからデータを取得
+        const jsonObj = localStorage.getItem('subjects');
+
+        // オブジェクトなのでJSON文字列から値をJSで使えるように変換
+        const jsObj = JSON.parse(jsonObj);
+        
+        // データが存在するのであればstateにsetする
+        if(jsonObj){
+            this.setState({ subjects: jsObj});
+        }
+    }
+
+    // ユーザーがページ使っている最中
+    componentDidUpdate(prevProps, prevState) {
+
+        // 配列の要素数を比べて、違ったら変更ありと判断
+        if(prevState.subjects.length !== this.state.subjects.length){
+
+            // データをJSON文字列に変換
+            const obj = JSON.stringify(this.state.subjects);
+
+            // localStorageにstateの変更をset
+            localStorage.setItem('subjects', obj);
+        }
+    }
+
     render() {
         return (
             <div className="study">

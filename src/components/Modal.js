3abf480e23ab3f1621fdd2b9ea1db52e9bf1c161
Kotaro456 <kotaro03281616@gmail.com>
2020-07-09 08:20:14 +0900

    changed modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,11 +4,34 @@ class Modal extends React.Component {
   render() {
     return (
       <div className="modal">
-            <div className="modal__content">
-              <h1 className="modal__content-subject">{this.props.subject}</h1>
-              <h2 className="modal__content-studyTime">{this.props.studyTime}</h2>
-              <button className="modal__content-button" onClick={this.props.handleCloseModal}>とじる</button>
-            </div>
+        {this.props.subject === undefined ||
+        this.props.studyTime === undefined ? (
+          <div className="modal__content">
+            <h1 className="modal__content-subject">
+              教科を1つ以上設定してください
+            </h1>
+            <h2 className="modal__content-studyTime">
+              時間を1つ以上設定してください
+            </h2>
+            <button
+              className="modal__content-button"
+              onClick={this.props.handleCloseModal}
+            >
+              とじる
+            </button>
+          </div>
+        ) : (
+          <div className="modal__content">
+            <h1 className="modal__content-subject">{this.props.subject}</h1>
+            <h2 className="modal__content-studyTime">{this.props.studyTime}</h2>
+            <button
+              className="modal__content-button"
+              onClick={this.props.handleCloseModal}
+            >
+              とじる
+            </button>
+          </div>
+        )}
       </div>
     );
   }
